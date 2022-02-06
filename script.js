5d2a1fcd0a005e3b2a8ea39f14348e32084b0e31
ivandro1981 <tcbmulticloud.ivandro@gmail.com>
2022-02-06 04:10:42 -0300

    Listagem de Produtos

--- script.js
+++ script.js
@@ -12,14 +12,19 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// adicionando os produtos da  API do mercado livre na função
 function createProductItemElement({ sku, name, image }) {
+  const items = document.querySelector('.items');//modificado
   const section = document.createElement('section');
+  
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  items.appendChild(section);//modificado
 
   return section;
 }
@@ -40,4 +45,27 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+//Para deixar nosso programa estruturado, irei criar uma função para listar os produtos,
+//Utiizando *fetch para capturar informações da API
+function listarProdutos() {
+  //realizar chamada através do fetch() ->promessa, desta forma teremos que utilizar o .then para obter
+  // os valores(informações) que desejamos, pois o fetch é assincrono
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((r) => r.json()) //transformando em formato json
+    .then((json) => { //manipular/obter informações
+      const Resultsjson = json.results;
+      console.log(Resultsjson);
+      Resultsjson.forEach((computador) => {
+       createProductItemElement({
+          sku: computador.id, 
+          name: computador.title, 
+          image: computador.thumbnail 
+        });
+      });
+  });
+}
+
+//Listar produtos(computadores)
+listarProdutos();
+
+window.onload = () => {}; 
